15854bee5435694879dba0bf437c802985225d26
liuqingqing <liuqingqing95@163.com>
2022-01-07 16:03:56 +0800

    feat: 个人信息

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -1,13 +1,13 @@
-const mysql = require('think-model-mysql');
+const mysql = require("think-model-mysql");
 
 module.exports = {
-    handle: mysql,
-    database: 'hiolabsDB',
-    prefix: 'hiolabs_',
-    encoding: 'utf8mb4',
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: '123123123',
-    dateStrings: true
+  handle: mysql,
+  database: "hiolabsdb",
+  prefix: "hiolabs_",
+  encoding: "utf8mb4",
+  host: "127.0.0.1",
+  port: "3306",
+  user: "root",
+  password: "123456",
+  dateStrings: true,
 };
